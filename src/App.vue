c70a240f7272a1a17ecd685f1a2ed8ab00735e4e
ricardojonathanromero <ricardo.jonathan.romero@gmail.com>
2020-11-16 15:04:32 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <NavBar />
+  <br>
+  <Welcome />
+  <br>
+  <Footer />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import NavBar from './components/NavBar.vue';
+import Welcome from './components/Welcome.vue';
+import Footer from './components/Footer.vue';
+
+@Options({
+  components: {
+    NavBar,
+    Welcome,
+    Footer
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
